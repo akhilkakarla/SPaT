355689a63497d64e61fcf6862c37c8a04725f004
jcr4698 <janrubio@utexas.edu>
2026-02-18 11:12:11 -0600

    Adding code to get SPaT response

---
+++ app/test/README.md
@@ -0,0 +1,11 @@
+# Test the SPaT Response
+
+## Dependencies
+```bash
+$ npm install -g tsx
+```
+
+## Fetching SPaT
+```bash
+$ tsx get_spat.tsx
+```
---
+++ app/test/get_spat.tsx
@@ -0,0 +1,5 @@
+
+// fetch the current SPaT
+fetch('http://129.114.36.77:8080/spat')
+.then(response => response.json())
+.then(json => console.log(json))
